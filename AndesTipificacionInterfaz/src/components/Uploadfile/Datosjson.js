430f6a028b23c97775a58f2a2496e510470943f6
Inter-telco-Luis <luis.rosero@inter-telco.com>
2020-12-01 14:55:48 -0500

    Se Agrega cambios para adaptar la interfaz de React al nuevo servicio que retornara los datos del cliente organizados

--- AndesTipificacionInterfaz/src/components/Uploadfile/Datosjson.js
+++ AndesTipificacionInterfaz/src/components/Uploadfile/Datosjson.js
@@ -5,7 +5,7 @@ import {
   Header,
   Table,
   Input,
-  TableBody,
+  // TableBody,
 } from "semantic-ui-react";
 
  
@@ -18,19 +18,30 @@ class Datosjson extends Component {
   
       this.state = {
         // objeto con las propiedades que necesitemos
-        texto:"",
-        tipificacion:[]
+        textoMensage:"",
+        textoDatosCliente:"",
+        tipificacion:[],
+        datosCliente:[]
       };
       this.handleChange = this.handleChange.bind(this);
+      this.dataUpClient = this.dataUpClient.bind(this);      
       this.onTextUpload = this.onTextUpload.bind(this);
+      this.service2Tipificacion = this.service2Tipificacion.bind(this);
     }
   
     handleChange(e) {
       const {value } = e.target;
-      // console.log(value)
       this.setState(
         {
-          texto:value
+          textoMensage:value
+        })  
+    }
+
+    dataUpClient(e) {
+      const {value } = e.target;
+      this.setState(
+        {
+          textoDatosCliente:value
         })  
     }
 
@@ -39,8 +50,8 @@ class Datosjson extends Component {
       // Create an object of formData
       const formData = new FormData(); 
       // Se forma las etiquetas de la peticion
-      formData.append("datos",this.state.texto);  
-      console.log(this.state.texto); 
+      formData.append("datos",this.state.textoMensage);  
+      // console.log(this.state.texto); 
       // Envio de peticion al servidor
       axios.post("http://54.234.133.155:5001", formData)
       .then(response => {
@@ -52,46 +63,51 @@ class Datosjson extends Component {
       });
 
       }; 
+
+      service2Tipificacion = () => {
+        // Create an object of formData
+        const formData = new FormData(); 
+        // Se forma las etiquetas de la peticion
+        formData.append("datos",this.state.textoDatosCliente);   
+        // Envio de peticion al servidor
+        axios.post("http://54.234.133.155:5001", formData)
+        .then(response => {
+            this.setState({
+            datosCliente:  response.data,
+            });
+            // console.log(this.state.suma_grupoAC)
+            return response
+        });
+  
+        }; 
 	
 	render() { 
     // const { texto ="" } = this.state;
     const { tipificacion =[] } = this.state;
+    const { datosCliente =[] } = this.state;
     
         
 	return ( 
-        <>    
+        <>
         <Header as="h6"  textAlign="center">
           <Table>
             <Table.Row>
-              <Table.HeaderCell>
-                <h1>Mensage</h1>
-                {/* {console.log(items)} */}
-                <Input                
-                  defaultValue=""
-                  valor="valor"
-                  type= "text"
-                  size='big' 
-                  onChange={this.handleChange}
-                />
-                <button onClick={this.onTextUpload}> 
-                  Cargar
-                </button>
-              </Table.HeaderCell>
-
-            
+            <h1>Servicio para el Proceso de Tipificacion</h1>
+              Mensage: <Input defaultValue="" valor="valor" type= "text" size='big' placeholder = "Mensage" onChange={this.handleChange}/> <button onClick={this.onTextUpload}> Cargar </button>
             </Table.Row>
-            <TableBody>
-                <Table.Row>
-                  <Table.Cell>
-                    <Input
-                      value = {tipificacion}
-                      size='big'
-                    />
-                    
-                  </Table.Cell>
-                </Table.Row>
-
-              </TableBody>
+            <br/>
+            <Table.Row>
+              Resultado S1: <Input value = {tipificacion} size='big' placeholder = "Tipificacion"/>
+            </Table.Row>
+            <br/>
+            <Table.Row>
+              Datos Cliente: <Input defaultValue="" valor="valor" type= "text" size='big' placeholder = "Datos del Cliente Vtex" onChange={this.dataUpClient}/> <button onClick={this.service2Tipificacion}> Cargar </button>  
+            </Table.Row>  
+            <br/>
+            <Table.Row>
+              Resultado S2: <Input value = {datosCliente} size='big' placeholder="Datos del Cliente Dynamics"/>
+            </Table.Row>
+            <br/>
           </Table>
         </Header>
       </>
